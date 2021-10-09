da36ba7916a0e9450ca649ac10d29bb1fd83d506
okami <OneTrickPone@protonmail.com>
2021-10-09 12:21:32 +0100

    Updated init.sh to sort leasefile

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ fi
 dnsmasq --test
 
 # Launch dnsmasq
-dnsmasq -d --log-dhcp --log-debug -7 /config --dhcp-leasefile /data/dnsmasq.lease &
+dnsmasq -d --log-dhcp --log-debug -7 /config -l /data/dnsmasq.lease &
 
 # Trap Sigterm n stuff
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
