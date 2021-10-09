6271682b90aab7b4efeb10bce9f50cac45b73af2
okami <OneTrickPone@protonmail.com>
2021-10-09 11:52:19 +0100

    Added entrypoint handling

--- dockerfile
+++ dockerfile
@@ -4,9 +4,15 @@ LABEL maintainer "gecko-developer@oxide.one"
 
 RUN apk --no-cache add dnsmasq
 
-VOLUME /etc/dnsmasq
+RUN mkdir /config /tftp
 
+VOLUME /config /tftp
+
+COPY ./init.sh      /usr/bin
+
+# DNS
 EXPOSE 53 53/udp
+# UDP
 EXPOSE 69 69/udp
 
-ENTRYPOINT ["dnsmasq", "-k"]
+ENTRYPOINT ["init.sh"]
---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Exit on failure
+set -e
+
+# Check for files in directory
+if [ -n "$(find /config -prune -empty 2>/dev/null)" ]
+then
+  echo "No files found in /config! Using default dnsmasq"
+  cp /etc/dnsmasq.conf /config
+fi
+
+
+# Check the syntax of dnsmasq
+dnsmasq --test
+
+# Launch dnsmasq
+dnsmasq -d --log-dhcp --log-debug -7 /config &
+
+# Trap Sigterm n stuff
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+wait
