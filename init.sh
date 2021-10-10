5ec525c7b8d5ff96239d8850bacf9b6a8d718087
okami <OneTrickPone@protonmail.com>
2021-10-10 21:36:07 +0100

    Fixed cp

--- init.sh
+++ init.sh
@@ -3,10 +3,6 @@
 # Exit on failure
 set -e
 
-cp /usr/share/syslinux/efi64/syslinux.efi
-cp /usr/share/syslinux/efi64/ldlinux.e64
-cp /usr/share/syslinux/lpxelinux.0
-
 # Check the syntax of dnsmasq
 dnsmasq --test
 
