d5fb3018c1ac138f07bbd7f508e74955d64d089a
Okami Dash <okami@doubledash.org>
2022-03-12 16:44:26 +0000

    Added PXELINUX config

--- init.sh
+++ init.sh
@@ -3,6 +3,15 @@
 # Exit on failure
 set -e
 
+# Check for the pxelinux-cfg environment variable
+if [[ -z "${PXELINUX_CFG_DIR}" ]] && [[ -d "${PXELINUX_CFG_DIR}"]]; then
+  echo "$0: PXELINUX_CFG_DIR not set or does not exist. continuing"
+else
+  echo "$0: PXELINUX_CFG_DIR set, value: \"$PXELINUX_CFG_DIR\"
+  ln -s $PXELINUX_CFG_DIR /usr/share/syslinux/pxelinux.cfg
+fi
+
+
 # Check the syntax of dnsmasq
 dnsmasq --test
 
